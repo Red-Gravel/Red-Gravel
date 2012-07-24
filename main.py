cc1605b1a47caccc218a4829b3b848b83fb64261
Adam Reeve <adreeve@gmail.com>
2012-07-24 20:30:41 +1200

    Set window title and make it a bit bigger by default

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 from direct.showbase.ShowBase import ShowBase
+from pandac.PandaModules import WindowProperties
 
 from redgravel.menu import MainMenu
 
@@ -7,6 +8,12 @@ from redgravel.menu import MainMenu
 class Game(ShowBase):
     def __init__(self):
         ShowBase.__init__(self)
+
+        windowProperties = WindowProperties(self.win.getProperties())
+        windowProperties.setTitle("Red Gravel")
+        #windowProperties.setFullscreen(True)
+        windowProperties.setSize(1024, 768)
+        self.win.requestProperties(windowProperties)
 
         # Start the main menu,
         # which can start a game
